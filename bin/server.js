5b7e98b8db317d375732a0ecc24da4e226879ec3
Trygve Lie <github@trygve-lie.com>
2013-07-23 17:33:49 +0200

    Changed ci port

--- bin/server.js
+++ bin/server.js
@@ -6,8 +6,10 @@ var httpProxy   = require('http-proxy'),
     options     = {
         hostnameOnly: true,
         router: {
+            'maiden.io'     : '127.0.0.1:8001',
             'www.maiden.io' : '127.0.0.1:8001',
-            'ci.maiden.io'  : '127.0.0.1:8002'
+            'ci.maiden.io'  : '127.0.0.1:3000',
+            'r.maiden.io'   : '127.0.0.1:8001'
     }
 };
 
