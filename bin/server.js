218e79882383f0e123ffae300074b0d769eacde5
Trygve Lie <github@trygve-lie.com>
2013-07-28 18:16:53 +0200

    Added npm

--- bin/server.js
+++ bin/server.js
@@ -6,10 +6,8 @@ var httpProxy   = require('http-proxy'),
     options     = {
         hostnameOnly: true,
         router: {
-            'maiden.io'     : '127.0.0.1:8001',
-            'www.maiden.io' : '127.0.0.1:8001',
             'ci.maiden.io'  : '127.0.0.1:3000',
-            'r.maiden.io'   : '127.0.0.1:8001'
+            'npm.maiden.io' : '127.0.0.1:3001'
     }
 };
 
